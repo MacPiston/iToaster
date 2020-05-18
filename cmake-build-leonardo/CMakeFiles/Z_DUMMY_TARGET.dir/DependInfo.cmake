64e042257a73eb58a60fed910a8212dfc35cec6c
MacPiston <mcpiston@gmail.com>
2020-05-18 12:20:36 +0200

    PlatformIO

---
+++ cmake-build-leonardo/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/maciejzajecki/Documents/GitHub/iToaster/src/main.c" "/Users/maciejzajecki/Documents/GitHub/iToaster/cmake-build-leonardo/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/cores/arduino"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/variants/leonardo"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/libraries/EEPROM/src"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/libraries/HID/src"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/libraries/SPI/src"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/libraries/SoftwareSerial/src"
+  "/Users/maciejzajecki/.platformio/packages/framework-arduino-avr/libraries/Wire/src"
+  "/Users/maciejzajecki/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include"
+  "/Users/maciejzajecki/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include-fixed"
+  "/Users/maciejzajecki/.platformio/packages/toolchain-atmelavr/avr/include"
+  "/Users/maciejzajecki/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
