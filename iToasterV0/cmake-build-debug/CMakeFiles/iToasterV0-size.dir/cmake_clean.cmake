35c4fb37c8a434d89c4f13a3c8fd25a84ebcc03e
MacPiston <mcpiston@gmail.com>
2020-05-18 12:18:57 +0200

    PlatformIO

--- iToasterV0/cmake-build-debug/CMakeFiles/iToasterV0-size.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/iToasterV0-size"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/iToasterV0-size.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- iToasterV0/cmake-build-debug/CMakeFiles/leonardo_LiquidCrystal.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libleonardo_LiquidCrystal.a"
-)
